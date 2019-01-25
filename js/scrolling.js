600f7fd4ee2794034db2f88d077c86185e630e57
ordanax@ya.ru <T5ng&WwG4Ne@uZp1&^*19H8yP>
2019-01-26 02:08:40 +0500

    Delete

--- js/scrolling.js
+++
@@ -1,8 +0,0 @@
-// Плавная прокрутка. Скорость прокуртки определяется праметром 1500
-$(function(){
-        $("a[href^='#']").click(function(){
-                var _href = $(this).attr("href");
-                $("html, body").animate({scrollTop: $(_href).offset().top+"px"},1500);
-                return false;
-        });
-});
