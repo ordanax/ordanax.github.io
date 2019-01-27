5dd52d9508355b26d0adfb82ef8023982f0b3fdb
ordanax@ya.ru <T5ng&WwG4Ne@uZp1&^*19H8yP>
2019-01-28 02:36:43 +0500

    new

--- js/common.js
+++ js/common.js
@@ -41,4 +41,13 @@ $(function() {
 		custom: false
 	});
 
+	$('.main-menu li').removeClass('active');
+	var path = window.location.pathname;
+	$('.main-menu li a').each(function() {
+		var href = $(this).attr('href');
+		if(path.slice(1).substring(0, href.length) === href) {
+			$(this).parent('li').addClass('active');
+		}
+	});
+
 });
